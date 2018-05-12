c69987c21a2d712e1a346bf47ab3b2a136a6514e
daihaikuo <daihaikuo@163.com>
2018-05-12 21:37:33 +0800

    第五次推送

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -11,7 +11,7 @@ import { HeroService }  from '../hero.service';
   styleUrls: [ './hero-detail.component.css' ]
 })
 export class HeroDetailComponent implements OnInit {
-  @Input() hero: Hero;
+  @Input() hero: Hero; 
 
   constructor(
     private route: ActivatedRoute,
